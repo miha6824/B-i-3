b65a7941ace66560bee00d278968a85212108c5f
hmyng6824 <hmyng6824@example.com>
2024-06-18 09:20:04 +0700

    Description of your changes

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,74 +1,73 @@
-import React, { useState } from 'react'
-import { StyleSheet, Text, View, TouchableOpacity, Image, FlatList } from 'react-native'
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, TouchableOpacity, Image, FlatList } from 'react-native';
 
-export default function RootLayout() {
+const Comments = () => {
   const data = [
     {
       id: 1,
       image: 'https://bootdey.com/img/Content/avatar/avatar1.png',
-      name: 'Frank Odalthh',
-      comment:
-        'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor.',
+      name: 'Thông báo 1',
+      comment: 'ĐT Pháp khó nhọc đánh bại Áo 1-0 trong trận ra quân. HLV Deschamps nói về kết quả của trận đấu:.',
+      time: '18/06/2024, 9:58 am',
     },
     {
       id: 2,
       image: 'https://bootdey.com/img/Content/avatar/avatar6.png',
-      name: 'John DoeLink',
-      comment:
-        'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor.',
+      name: 'Thông báo 2',
+      comment: 'ĐT Pháp khó nhọc đánh bại Áo 1-0 trong trận ra quân. HLV Deschamps nói về kết quả của trận đấu:.',
+      time: '18/06/2024, 9:58 am',
     },
     {
       id: 3,
       image: 'https://bootdey.com/img/Content/avatar/avatar7.png',
-      name: 'March SoulLaComa',
-      comment:
-        'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor.',
+      name: 'Thông báo 3',
+      comment: 'ĐT Pháp khó nhọc đánh bại Áo 1-0 trong trận ra quân. HLV Deschamps nói về kết quả của trận đấu:.',
+      time: '18/06/2024, 9:58 am',
     },
     {
       id: 4,
       image: 'https://bootdey.com/img/Content/avatar/avatar2.png',
-      name: 'Finn DoRemiFaso',
-      comment:
-        'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor.',
+      name: 'Thông báo 4',
+      comment: 'ĐT Pháp khó nhọc đánh bại Áo 1-0 trong trận ra quân. HLV Deschamps nói về kết quả của trận đấu:.',
+      time: '18/06/2024, 9:58 am',
     },
     {
       id: 5,
       image: 'https://bootdey.com/img/Content/avatar/avatar3.png',
-      name: 'Maria More More',
-      comment:
-        'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor.',
+      name: 'Thông báo 5',
+      comment: 'ĐT Pháp khó nhọc đánh bại Áo 1-0 trong trận ra quân. HLV Deschamps nói về kết quả của trận đấu:.',
+      time: '18/06/2024, 9:58 am',
     },
     {
       id: 6,
       image: 'https://bootdey.com/img/Content/avatar/avatar4.png',
-      name: 'Clark June Boom!',
-      comment:
-        'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor.',
+      name: 'Thông báo 6',
+      comment: 'ĐT Pháp khó nhọc đánh bại Áo 1-0 trong trận ra quân. HLV Deschamps nói về kết quả của trận đấu:.',
+      time: '18/06/2024, 9:58 am',
     },
     {
       id: 7,
       image: 'https://bootdey.com/img/Content/avatar/avatar5.png',
-      name: 'The googler',
-      comment:
-        'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor.',
+      name: 'Thông báo 7',
+      comment: 'ĐT Pháp khó nhọc đánh bại Áo 1-0 trong trận ra quân. HLV Deschamps nói về kết quả của trận đấu:.',
+      time: '18/06/2024, 9:58 am',
     },
-  ]
+  ];
 
-  const [comments, setComments] = useState(data)
+  const [comments, setComments] = useState(data);
 
   return (
-    <FlatList
-      style={styles.root}
-      data={comments}
-      extraData={comments} // Sửa lỗi tại đây
-      ItemSeparatorComponent={() => {
-        return <View style={styles.separator} />
-      }}
-      keyExtractor={item => {
-        return item.id.toString() // Đảm bảo key là chuỗi
-      }}
-      renderItem={({ item }) => { // Destructuring item
-        return (
+    <View style={styles.mainContainer}>
+      <View style={styles.header}>
+        <Text style={styles.notificationText}>Thông báo</Text>
+      </View>
+      <FlatList
+        style={styles.root}
+        data={comments}
+        extraData={comments}
+        ItemSeparatorComponent={() => <View style={styles.separator} />}
+        keyExtractor={item => item.id.toString()}
+        renderItem={({ item }) => (
           <View style={styles.container}>
             <TouchableOpacity onPress={() => {}}>
               <Image style={styles.image} source={{ uri: item.image }} />
@@ -76,38 +75,41 @@ export default function RootLayout() {
             <View style={styles.content}>
               <View style={styles.contentHeader}>
                 <Text style={styles.name}>{item.name}</Text>
-                <Text style={styles.time}>9:58 am</Text>
               </View>
-              <Text style={styles.comment}>{item.comment}</Text>
-
+              <Text style={styles.commentText}>{item.comment}</Text>
+              <Text style={styles.time}>{item.time}</Text>
             </View>
           </View>
-        )
-      }}
-    />
-  )
-}
+        )}
+      />
+    </View>
+  );
+};
 
 const styles = StyleSheet.create({
- 
-    comment: {
-      fontSize: 14,
-      lineHeight: 20, // Điều chỉnh line height cho phù hợp
-      color: '#333333',
-      marginTop: 5, // Điều chỉnh margin top nếu cần thiết
-    },
-  
-  
+  mainContainer: {
+    flex: 1,
+    backgroundColor: '#ffffff',
+  },
+  header: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    height: 50, // Adjust height as needed
+    backgroundColor: '#ffffff',
+  },
+  notificationText: {
+    fontSize: 20,
+    fontWeight: 'bold',
+  },
   root: {
     backgroundColor: '#ffffff',
     marginTop: 10,
   },
   container: {
-    paddingLeft: 19,
-    paddingRight: 16,
-    paddingVertical: 12,
     flexDirection: 'row',
     alignItems: 'flex-start',
+    paddingVertical: 12,
+    paddingHorizontal: 10,
   },
   content: {
     marginLeft: 16,
@@ -125,15 +127,20 @@ const styles = StyleSheet.create({
   image: {
     width: 45,
     height: 45,
-    borderRadius: 22,
-    marginLeft: 20,
+    borderRadius: 22.5,
   },
   time: {
     fontSize: 11,
     color: '#808080',
+    marginTop: 4,
   },
   name: {
     fontSize: 16,
     fontWeight: 'bold',
   },
-})
+  commentText: {
+    fontSize: 14,
+  },
+});
+
+export default Comments;
